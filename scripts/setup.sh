597e2d27c9d3547a67012151ede64ed30af57553
sven <sven@so36.net>
2016-05-04 10:38:13 +0200

    Cleanup setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 # This script is used by vagrant to setup the test box
+
+# Run group install
 sudo dnf group install "C Development Tools and Libraries" -y
-sudo dnf install ruby-devel zlib-devel libxml2-devel libxslt-devel nodejs -y
-# FeedParser is used for RSS feed
-sudo dnf install python-feedparser python-pygments -y
-# spec dependencies for Capybara & Webkit testing
-sudo dnf install rubygem-rack rubygem-capybara rubygem-rspec -y
-sudo dnf install redhat-rpm-config patch -y
-# We need Jekyll 3.0 because of hooking rss.py script into the build itself
+
+# Install all the sytem packages we need, this includes also all dependencies for ruby and Jekyll
+# List of packages
+PACKAGES="ruby-devel zlib-devel libxml2-devel libxslt-devel redhat-rpm-config patch nodejs python-feedparser python-pygments rubygem-rack rubygem-capybara rubygem-rspec"
+
+# Install them
+sudo dnf install $PACKAGES -y
+
+# We need at least Jekyll 3.0 because of hooking rss.py script into the build itself
 # but beta release because of compatibility with jekyll-lunr-js-search
 # plugin, see https://github.com/slashdotdash/jekyll-lunr-js-search/pull/85
 gem uninstall jekyll || :
